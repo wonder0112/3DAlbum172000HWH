8b565a366fea12fc8bc6cc78f7fd7063efe19c93
wonder0112 <hwhgdcn@gmail.com>
2019-03-29 15:28:55 +0800

    文字动画效果

--- app/src/main/java/cn/edu/gdpt/xxgcx/a3dalbum172000hwh/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/xxgcx/a3dalbum172000hwh/MainActivity.java
@@ -2,7 +2,13 @@ package cn.edu.gdpt.xxgcx.a3dalbum172000hwh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.itheima.coverflow.ui.FeatureCoverFlow;
 
@@ -22,11 +28,32 @@ public class MainActivity extends AppCompatActivity {
         initView();//调用函数
         initData();
         initCoverFlow();
+        initAnimation();
     }
+
+    private void initAnimation() {
+        //文字出现和退出的动画控制器
+        Animation in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_top);
+        Animation out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_bottom);
+        title.setInAnimation(in);
+        title.setOutAnimation(out);
+    }
+
     private void initCoverFlow() {
         AlbumAdapter adapter = new AlbumAdapter(getApplicationContext());
         adapter.setData(dataList);
         coverflow.setAdapter(adapter);
+        coverflow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                String[] name = {"Gril","Spring Scenery","Summer Scenery","Autum Scenery","Winter Scenery"};
+                title.setText(name[position]);
+            }
+            @Override
+            public void onScrolling() {
+
+            }
+        });
     }
 
     private void initData() {
@@ -49,5 +76,16 @@ public class MainActivity extends AppCompatActivity {
         coverflow.setRotationTreshold(0.5f);
         coverflow.setScalingThreshold(0.5f);
         coverflow.setSpacing(0.6f);
+//findViewById()查找界面上的控件  LayoutInflater 查找布局文件  LayoutInflater。setFactory查找布局文件
+      //将TextSwitch与item_title里的TextView控件进行捆绑
+        title.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+                TextView view = (TextView) inflater.inflate(R.layout.item_title, null);
+                return view;
+            }
+        });
+
     }
 }
